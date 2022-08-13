85ea572c605b200ffcd279088d4509a0d14e5894
MAK <mak2022official@gmail.com>
2022-08-13 23:34:51 +0600

    install firebase & add signin page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Routes, Route, Outlet } from 'react-router-dom';
 
 import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
+
 import Home from './routes/home/home.component';
 
 
@@ -17,6 +19,7 @@ const App = () => {
       <Route path='/' element={<Navigation />} >
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
+        <Route path='signin' element={<SignIn />} />
       </Route>
     </Routes>
   );
